4a16c588b65900515b3615c5b9532dd901c8a825
ahmed-faqih <j.ahmedfaqih@gmail.com>
2026-03-27 00:33:06 +0300

    Update Zone.java

--- src/Zone.java
+++ src/Zone.java
@@ -13,7 +13,7 @@ public class Zone implements Comparable<Zone>{
 
 
 
-    public Zone(String ID,String population,String urgency, String distance, String supply, String winStart, String winEnd){
+    public Zone(String ID, String population, String urgency, String distance, String supply, String winStart, String winEnd, double totalC, double totalD) {
         this.ID = ID;
         this.population = Integer.parseInt(population);
         this.urgency = Double.parseDouble(urgency);
@@ -21,18 +21,11 @@ public class Zone implements Comparable<Zone>{
         this.supply = Integer.parseInt(supply);
         this.winStart = LocalDateTime.parse(winStart);
         this.winEnd = LocalDateTime.parse(winEnd);
-        this.utility = this.ComputeUtility();
+        this.utility = this.ComputeUtility(totalC, totalD); 
     }
 
-    public double ComputeUtility() {
-        
-        double C = 11001.0;
-        double D = 62733.0;
-        
-        // Calculate the percentage of truck limits this zone consumes (round-trip distance)
+    public double ComputeUtility(double C, double D) {
         double costPercentage = (this.supply / C) + ((this.distance * 2.0) / D);
-        
-        // Return the Resource-Consumption Hybrid Score
         return (this.population * this.urgency) / costPercentage;
     }
 
