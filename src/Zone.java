9c8b1be66007ccead92090ad2b54eea9482767f2
ahmed-faqih <j.ahmedfaqih@gmail.com>
2026-03-26 19:39:23 +0300

    Update Zone.java
    Added Utility Score
    Fix on the priorityQueue to make it a Max-heap

--- src/Zone.java
+++ src/Zone.java
@@ -24,14 +24,23 @@ public class Zone implements Comparable<Zone>{
         this.utility = this.ComputeUtility();
     }
 
-    public double ComputeUtility(){
-        //return this.population*this.urgency;
-        return Math.pow(this.population*this.urgency,2)/distance;
+    public double ComputeUtility() {
+        
+        double C = 11001.0;
+        double D = 62733.0;
+        
+        // Calculate the percentage of truck limits this zone consumes (round-trip distance)
+        double costPercentage = (this.supply / C) + ((this.distance * 2.0) / D);
+        
+        // Return the Resource-Consumption Hybrid Score
+        return (this.population * this.urgency) / costPercentage;
     }
 
     @Override
-    public int compareTo(Zone obj){
-        return (int)(this.utility-obj.utility);
+    public int compareTo(Zone obj) {
+        // FIX: Use Double.compare and reverse the order (obj vs this) 
+        // to make the PriorityQueue a Max-Heap
+        return Double.compare(obj.utility, this.utility);
     }
     
     @Override
