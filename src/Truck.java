3067288a34718a684585c85b9fb64af47b780bd6
ahmed-faqih <j.ahmedfaqih@gmail.com>
2026-03-27 05:48:31 +0300

    Update Truck.java

--- src/Truck.java
+++ src/Truck.java
@@ -1,10 +1,26 @@
 public class Truck {
     int capacity;
-    int totRange; // total travel range
+    double totRange;
 
-    public Truck(int capacity, int totRange){
+    int currentSupplies;
+    double remainingRange;
+
+    public Truck(int capacity, double totRange) {
         this.capacity = capacity;
         this.totRange = totRange;
+        
+        this.currentSupplies = capacity;
+        this.remainingRange = totRange;
     }
 
+    public boolean canMakeTrip(int supplyNeeded, double distanceNeeded) {
+        boolean hasSupplies = this.currentSupplies >= supplyNeeded;
+        boolean hasFuel = this.remainingRange >= distanceNeeded;
+        return hasSupplies && hasFuel;
+    }
+
+    public void deliver(int supplyDelivered, double distanceDriven) {
+        this.currentSupplies -= supplyDelivered;
+        this.remainingRange -= distanceDriven;
+    }
 }
