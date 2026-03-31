c9f02d33fb33c4df3395672b5d1bc8d02254e792
So-Almarshad <223110773@psu.edu.sa>
2026-03-31 16:54:20 +0300

    Fully functional DP souloutin

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import java.time.LocalDateTime;
 
 
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Zone> list = listLoadData();
-        System.out.println(DynamicProgramming(list));
+        long startTime = System.currentTimeMillis();
+        DynamicProgramming(list);
+        long endTime = System.currentTimeMillis(); 
+        System.out.println("DP Execution Time: " + (endTime - startTime) + " ms");
+        
     }
     
     public static PriorityQueue<Zone> loadData() {
@@ -57,37 +61,55 @@ public class Main {
         int numZones = list.size();
         int maxDistance = 0;
         int maxSupply = 0;
+        double maxPossibleUtility = 0;
+        LocalDateTime earliestTime =list.get(0).winStart;
         for (Zone zone : list) {
             if (zone.distance>maxDistance)
                 maxDistance = (int)Math.ceil(zone.distance);
             if (zone.supply>maxSupply)
                 maxSupply=(int)Math.ceil(zone.supply);
+            if (zone.winStart.isBefore(earliestTime))
+                earliestTime=zone.winStart;
+            maxPossibleUtility+= zone.utility;
         }
+        LocalDateTime currentTime = earliestTime;
+        System.out.println("--- STARTING DYNAMIC PROGRAMMING ---");
         double[][][] benefit = new double[numZones][maxDistance][maxSupply];
-
         for (int zone = 0 ; zone <numZones; zone++) {
             for (int distance = 0; distance < maxDistance; distance++) {
                 for (int supply = 0; supply < maxSupply; supply++) {
                     int currentSupply = (int) Math.ceil(list.get(zone).supply);
                     int currentDistance = (int) Math.ceil(list.get(zone).distance);
                     double currentUtility = list.get(zone).utility;
-
+                    LocalDateTime winStart = list.get(zone).winStart;
+                    LocalDateTime winEnd = list.get(zone).winEnd;
+                    int timeToVisit = (int) Math.ceil(currentDistance/60);
                     if (zone==0){// fill first column
-                            if (currentDistance <= distance & currentSupply <= supply){
-                                benefit[zone][distance][supply] = currentUtility;
-                            }
+                        if (currentDistance <= distance & currentSupply <= supply){
+                            benefit[zone][distance][supply] = currentUtility;
+                        }
                     }
-                    else if (currentSupply<=supply & currentDistance<=distance){
+                    else if (currentSupply<=supply & currentDistance<=distance ){
+                        //check if access window allows.
+                        if (currentTime.plusHours(timeToVisit).isAfter(winStart) & currentTime.plusHours(timeToVisit).isBefore(winEnd)) {
                             // check if current zone is worth visiting
                             if (currentUtility + benefit[zone-1][distance-currentDistance][supply-currentSupply] > benefit[zone-1][distance][supply]) {
                                 benefit[zone][distance][supply] = currentUtility + benefit[zone-1][distance-currentDistance][supply-currentSupply];
+                                currentTime = currentTime.plusHours(timeToVisit);
                             } else
                                 benefit[zone][distance][supply] = benefit[zone-1][distance][supply];
+                        } else
+                            benefit[zone][distance][supply] = benefit[zone-1][distance][supply];
+                        
                     } else // zone cannot be visted
-                        benefit[zone][distance][supply] = benefit[zone-1][distance][supply];   
-                }
+                    benefit[zone][distance][supply] = benefit[zone-1][distance][supply]; 
+            }
             }
         }
+       double utilityScore = (benefit[numZones-1][maxDistance-1][maxSupply-1] / maxPossibleUtility) * 100.0;
+        System.out.println("\n--- RESULTS ---");
+        System.out.printf("Utility Score: %.2f / 100\n", utilityScore);
         return benefit[numZones-1][maxDistance-1][maxSupply-1];
+        
     }
 }
