005e532513a8a9e49011014e5b7e654c8f98e64e
ahmed-faqih <j.ahmedfaqih@gmail.com>
2026-04-01 19:39:57 +0300

    Update Truck.java

--- src/Truck.java
+++ src/Truck.java
@@ -1,24 +1,28 @@
 public class Truck {
     int capacity;
-    double totRange;
+    double totRange; // Changed to double for decimal precision!
+
+    // Trackers for the truck's current state during the route
     int currentSupplies;
     double remainingRange;
-
 
     public Truck(int capacity, double totRange) {
         this.capacity = capacity;
         this.totRange = totRange;
         
+        // When the truck is created, it starts fully loaded and fully fueled
         this.currentSupplies = capacity;
         this.remainingRange = totRange;
     }
 
+    // Checks if the truck has enough supplies and range for this specific zone
     public boolean canMakeTrip(int supplyNeeded, double distanceNeeded) {
         boolean hasSupplies = this.currentSupplies >= supplyNeeded;
         boolean hasFuel = this.remainingRange >= distanceNeeded;
         return hasSupplies && hasFuel;
     }
 
+    // Updates the truck's inventory and odometer after a delivery
     public void deliver(int supplyDelivered, double distanceDriven) {
         this.currentSupplies -= supplyDelivered;
         this.remainingRange -= distanceDriven;
